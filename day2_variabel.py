5d69be23efb44d146e3f006eaa65c90bef06ef4e
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-12 19:21:21 +0800

    Add files via upload

---
+++ day2_variabel.py
@@ -0,0 +1,15 @@
+#variabel
+#variabel adalah wadah atau tempat menampung data
+a=10
+#maka nilai a=10
+print(a)
+#untuk memanggil nilai a menggunakan fungsi print
+#NOTE: ketika print type string itu menggunakan kutip 2 (""), namun ketika print variabel tanpa kutip 2
+#contoh
+name="Rahmawati"
+print("my name is",name)
+#variabel digunakan ketika ada 1 data yang masi digunakan untuk memperoses atau di ulang-ulang
+#contoh
+name= "aku"
+print(name,"sejenis sayuran",name,"punya rasa yang manis,segar, dan enak",name,"bisa membuat matamu sehat dan matamu kuat. Apalagi",
+      "rambut dan kulitmu",name,"lah yang merawatnya. siapakah ",name,"?")
